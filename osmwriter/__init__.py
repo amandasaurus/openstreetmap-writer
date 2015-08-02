4c9ccfb643e954a5bfc2b6c77ea981e72dbd4022
Rory McCann <rory@technomancy.org>
2015-08-02 14:31:45 +0200

    support relations

--- osmwriter/__init__.py
+++ osmwriter/__init__.py
@@ -67,6 +67,36 @@ class OSMWriter(object):
         self.xmlfile.endElement("way")
 
 
+    def relation(self, id, tags, members, **metadata):
+        tags = tags or {}
 
+        self.xmlfile.characters("\n  ")
+        attrs = {'id': t(id)}
+        for key, value in metadata.items():
+            attrs[key] = t(value)
+
+        self.xmlfile.startElement("relation", attrs)
+
+        for member in members:
+            attrs = {}
+            if len(member) == 2:
+                type, ref = member
+                attrs = {'type': t(type), 'ref': t(ref)}
+            else:
+                type, ref, role = member
+                attrs = {'type': t(type), 'ref': t(ref), 'role': t(role)}
+            self.xmlfile.characters("\n    ")
+            self.xmlfile.startElement("member", attrs)
+            self.xmlfile.endElement("member")
+
+
+        for key, value in tags.items():
+            self.xmlfile.characters("\n    ")
+            self.xmlfile.startElement("tag", {'k': key, 'v': value})
+            self.xmlfile.endElement("tag")
+
+
+        self.xmlfile.characters("\n  ")
+        self.xmlfile.endElement("relation")
 
 
